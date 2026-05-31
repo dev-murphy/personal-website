2025af657bcaa05a1245bbcdac9c4cce1a7587d5
dev-murphy <facey.murphy1@gmail.com>
2026-05-30 21:17:36 -0500

    chore: add blogs folder and schema to content config

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,18 @@
-import { defineContentConfig, defineCollection } from "@nuxt/content";
+import { defineContentConfig, defineCollection, z } from "@nuxt/content";
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection({
+    blog: defineCollection({
       type: "page",
-      source: "**/*.md",
+      source: "blog/**/*.md",
+      schema: z.object({
+        date: z.string(),
+        lastEdited: z.string().optional(),
+        image: z.string().optional(),
+        category: z.string().optional(),
+        duration: z.string().optional(),
+        tags: z.array(z.string()).optional(),
+      }),
     }),
   },
 });
