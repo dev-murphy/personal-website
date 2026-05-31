c7a7ce00fda5fbaf7f3845e034f57fa3de8501c4
dev-murphy <facey.murphy1@gmail.com>
2026-05-31 09:58:04 -0500

    fix(app): wrapped theme toggle in client only to prevent hydration mishaps

--- app/components/x/ThemeToggle.vue
+++ app/components/x/ThemeToggle.vue
@@ -3,18 +3,28 @@ const { isDark, toggleTheme } = useTheme();
 </script>
 
 <template>
-  <button
-    class="w-9.5 bg-secondary border border-border-100 p-0.5 rounded-full"
-    @click="toggleTheme"
-  >
-    <div class="relative w-full h-4">
+  <ClientOnly>
+    <button
+      class="w-9.5 bg-secondary border border-border-100 p-0.5 rounded-full"
+      @click="toggleTheme"
+    >
+      <div class="relative w-full h-4">
+        <div
+          class="absolute w-4 h-4 bg-text-100 rounded-full"
+          style="transition: transform 300ms ease-in-out"
+          :style="{
+            transform: isDark ? 'translateX(16px)' : 'translateX(0)',
+          }"
+        ></div>
+      </div>
+    </button>
+
+    <template #fallback>
       <div
-        class="absolute w-4 h-4 bg-text-100 rounded-full"
-        style="transition: transform 300ms ease-in-out"
-        :style="{
-          transform: isDark ? 'translateX(16px)' : 'translateX(0)',
-        }"
-      ></div>
-    </div>
-  </button>
+        class="w-9.5 bg-secondary border border-border-100 p-0.5 rounded-full"
+      >
+        <div class="h-4" />
+      </div>
+    </template>
+  </ClientOnly>
 </template>
